a84281114c2e6aed14b0990e99e34474f356c158
Eric Ramming <eric.ramming@raytheon.com>
2024-12-04 08:15:18 -0500

    More tokenizer tests

--- src/tokens/Tokenizer.py
+++ src/tokens/Tokenizer.py
@@ -2,6 +2,7 @@ from .Token import Token
 from .QueryTerm import QueryTerm
 from src.LookaheadQueue import LookaheadQueue
 import re
+from re import Match
 
 
 class Tokenizer:
@@ -38,6 +39,21 @@ class Tokenizer:
         elif first_letter == ":":
             tokens.put(Token(QueryTerm.COLON))
             remainder: str = query_str[1:].lstrip()
+            self.tokenize_helper(remainder, tokens)
+        elif first_letter == ".":
+            tokens.put(Token(QueryTerm.PERIOD))
+            remainder: str = query_str[1:].lstrip()
+            self.tokenize_helper(remainder, tokens)
+        elif first_letter == ";":
+            tokens.put(Token(QueryTerm.SEMI_COLON))
+            remainder: str = query_str[1:].lstrip()
+            self.tokenize_helper(remainder, tokens)
+        elif first_letter == "*":
+            tokens.put(Token(QueryTerm.ASTERISK))
+            remainder: str = query_str[1:].lstrip()
+            self.tokenize_helper(remainder, tokens)
+        elif first_letter == '"' or first_letter == "'":
+            remainder: str = self.string_literal_tokenizer(query_str, tokens)
             self.tokenize_helper(remainder, tokens)
         elif first_letter == "#":
             indx: int = re.search("(\r\n|\n)", query_str).end()
@@ -86,29 +102,35 @@ class Tokenizer:
             tokens.put(Token(QueryTerm.GRAPH))
             remainder: str = query_str[5:].lstrip()
             self.tokenize_helper(remainder, tokens)
+        elif re.search("^(\\+?|-?)[0-9]+(\\.[0-9]*)?", query_str):
+            remainder: str = self.number_literal_tokenizer(query_str, tokens)
+            self.tokenize_helper(remainder, tokens)
         else:
             remainder: str = self.prefixed_name_prefix_tokenizer(query_str, tokens)
             self.tokenize_helper(remainder, tokens)
 
     def iri_ref_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
-        iri_ref: str = re.search("(^<[a-z0-9A-Z:_\\-#%\\.\\/]+>)", query_str).__getitem__(0)
-        if not iri_ref:
+        match: Match = re.search("(^<[a-z0-9A-Z:_\\-#%\\.\\/]+>)", query_str)
+        if not match:
             raise ValueError("Invalid IRIREF")
+        iri_ref: str = match.groups()[0]
         tokens.put(Token(QueryTerm.IRIREF, iri_ref[1:len(iri_ref) - 1]))
         return query_str[len(iri_ref):].lstrip()
 
     def variable_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
-        variable: str = re.search("(^[a-z0-9A-Z_]+)", query_str).__getitem__(0)
-        if not variable:
+        match: Match = re.search("(^[a-z0-9A-Z_]+)", query_str)
+        if not match:
             raise ValueError("Invalid variable")
+        variable: str = match.groups()[0]
         tokens.put(Token(QueryTerm.VARIABLE, variable))
         return query_str[len(variable):].lstrip()
     
     # Always before a colon
     def prefixed_name_prefix_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
-        prefix_name: str = re.search("(^[a-zA-Z_][a-zA-Z_0-9//.//-]*[a-zA-Z_0-9//-]?:)", query_str).__getitem__(0)
-        if not prefix_name:
+        match: Match = re.search("(^[a-zA-Z_][a-zA-Z_0-9\\.\\-]*[a-zA-Z_0-9\\-]?:)", query_str)
+        if not match:
             raise ValueError("Invalid prefixed name prefix")
+        prefix_name: str = match.groups()[0]
         tokens.put(Token(QueryTerm.PREFIXED_NAME_PREFIX, prefix_name[0:len(prefix_name) - 1]))
         tokens.put(Token(QueryTerm.COLON))
 
@@ -124,10 +146,41 @@ class Tokenizer:
     # Always after a colon
     def prefixed_name_local_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
         pattern: str = "(^([a-zA-Z_:0-9]|%[0-9A-Fa-f]{2})"
-        pattern += "([a-zA-Z_:0-9//-//.]|%[0-9A-Fa-f]{2})*"
-        pattern += "([a-zA-Z_:0-9//-]|%[0-9A-Fa-f]{2})?)"
-        local_name: str = re.search(pattern, query_str).__getitem__(0)
-        if not local_name:
+        pattern += "([a-zA-Z_:0-9\\-\\.]|%[0-9A-Fa-f]{2})*"
+        pattern += "([a-zA-Z_:0-9\\-]|%[0-9A-Fa-f]{2})?)"
+        match: Match = re.search(pattern, query_str)
+        if not match:
             raise ValueError("Invalid prefixed_name_local")
+        local_name: str = match.groups()[0]
         tokens.put(Token(QueryTerm.PREFIXED_NAME_LOCAL, local_name))
         return query_str[len(local_name):].lstrip()
+    
+    def string_literal_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
+        pattern: str = ""
+        quote_count: int = None
+        if query_str[0:3] == "'''":
+            pattern = "^'''(.+)'''"
+            quote_count = 6
+        elif query_str[0] == "'":
+            pattern = "^'(.+)'"
+            quote_count = 2
+        elif query_str[0:3] == '"""':
+            pattern = '^"""(.+)"""'
+            quote_count = 6
+        else:
+            pattern = '^"(.+)"'
+            quote_count = 2
+        match: Match = re.search(pattern, query_str)
+        if not match:
+            raise ValueError("Invalid string literal")
+        literal: str = match.groups()[0]
+        tokens.put(Token(QueryTerm.STRING_LITERAL, literal))
+        return query_str[len(literal) + quote_count:].lstrip()
+    
+    def number_literal_tokenizer(self, query_str: str, tokens: LookaheadQueue) -> str:
+        match: Match = re.search("^((+?|-?)[0-9]+(\\.[0-9]*)?)", query_str)
+        if not match:
+            raise ValueError("Invalid number literal")
+        num_as_string: str = match.groups()[0]
+        tokens.put(Token(QueryTerm.NUMBER_LITERAL, num_as_string))
+        return query_str[len(num_as_string):].lstrip()
--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -28,13 +28,103 @@ def test_tokenizer_prologue() -> None:
     assert tokens.get(block=False).term == QueryTerm.EOF
 
 def test_tokenizer_select_variables() -> None:
-    raise NotImplementedError()
+    query_str: str = "SELECT ?first_name ?_AGE ?4th_grade_teacher"
+    tokenizer: Tokenizer = Tokenizer()
+    tokens: LookaheadQueue = tokenizer.tokenize(query_str)
+
+    assert tokens.get(block=False).term is QueryTerm.SELECT
+    var1: Token = tokens.get(block=False)
+    assert var1.term == QueryTerm.VARIABLE
+    assert var1.content == "first_name"
+    var2: Token = tokens.get(block=False)
+    assert var2.term == QueryTerm.VARIABLE
+    assert var2.content == "_AGE"
+    var3: Token = tokens.get(block=False)
+    assert var3.term == QueryTerm.VARIABLE
+    assert var3.content == "4th_grade_teacher"
+    assert tokens.get(block=False).term is QueryTerm.EOF
+
+def test_tokenizer_select_distinct() -> None:
+    query_str: str = "SELECT DISTINCT ?first_name"
+    tokenizer: Tokenizer = Tokenizer()
+    tokens: LookaheadQueue = tokenizer.tokenize(query_str)
+
+    assert tokens.get(block=False).term is QueryTerm.SELECT
+    assert tokens.get(block=False).term is QueryTerm.DISTINCT
+    var1: Token = tokens.get(block=False)
+    assert var1.term == QueryTerm.VARIABLE
+    assert var1.content == "first_name"
+    assert tokens.get(block=False).term is QueryTerm.EOF
+
+def test_tokenizer_select_all() -> None:
+    query_str: str = "SELECT *"
+    tokenizer: Tokenizer = Tokenizer()
+    tokens: LookaheadQueue = tokenizer.tokenize(query_str)
+
+    assert tokens.get(block=False).term is QueryTerm.SELECT
+    assert tokens.get(block=False).term is QueryTerm.ASTERISK
+    assert tokens.get(block=False).term is QueryTerm.EOF
 
 def test_tokenizer_dataset_clause() -> None:
-    raise NotImplementedError()
+    query_str: str = "FROM NAMED ex:MyGraph-uuid"
+    tokenizer: Tokenizer = Tokenizer()
+    tokens: LookaheadQueue = tokenizer.tokenize(query_str)
 
-def test_tokenizer_skeleton_query() -> None:
-    raise NotImplementedError()
+    assert tokens.get(block=False).term is QueryTerm.FROM
+    assert tokens.get(block=False).term is QueryTerm.NAMED
+    prefix_name: Token = tokens.get(block=False)
+    assert prefix_name.term == QueryTerm.PREFIXED_NAME_PREFIX
+    assert prefix_name.content == "ex"
+    assert tokens.get(block=False).term == QueryTerm.COLON
+    local_name: Token = tokens.get(block=False)
+    assert local_name.term == QueryTerm.PREFIXED_NAME_LOCAL
+    assert local_name.content == "MyGraph-uuid"
+    assert tokens.get(block=False).term is QueryTerm.EOF
+
+def test_tokenizer_full_query() -> None:
+    query_str: str = '''
+        prefix foaf: <http://xmlns.com/foaf/0.1/>
+        
+        # An example query
+        SELECT *
+        WHERE {
+            ?person foaf:givenName "Eric" .
+        }
+        '''
+    tokenizer: Tokenizer = Tokenizer()
+    tokens: LookaheadQueue = tokenizer.tokenize(query_str)
+
+    assert tokens.get(block=False).term is QueryTerm.PREFIX
+    prefix_name: Token = tokens.get(block=False)
+    assert prefix_name.term == QueryTerm.PREFIXED_NAME_PREFIX
+    assert prefix_name.content == "foaf"
+    assert tokens.get(block=False).term == QueryTerm.COLON
+    iriref: Token = tokens.get(block=False)
+    assert iriref.term == QueryTerm.IRIREF
+    assert iriref.content == "http://xmlns.com/foaf/0.1/"
+
+    assert tokens.get(block=False).term is QueryTerm.SELECT
+    assert tokens.get(block=False).term is QueryTerm.ASTERISK
+    assert tokens.get(block=False).term is QueryTerm.WHERE
+    assert tokens.get(block=False).term is QueryTerm.LBRACKET
+    var1: Token = tokens.get(block=False)
+    assert var1.term == QueryTerm.VARIABLE
+    assert var1.content == "person"
+    prefix_name: Token = tokens.get(block=False)
+    assert prefix_name.term == QueryTerm.PREFIXED_NAME_PREFIX
+    assert prefix_name.content == "foaf"
+    assert tokens.get(block=False).term is QueryTerm.COLON
+    local_name: Token = tokens.get(block=False)
+    assert local_name.term == QueryTerm.PREFIXED_NAME_LOCAL
+    assert local_name.content == "givenName"
+    string_literal: Token = tokens.get(block=False)
+    assert string_literal.term == QueryTerm.STRING_LITERAL
+    assert string_literal.content == "Eric"
+
+    assert tokens.get(block=False).term is QueryTerm.PERIOD
+    assert tokens.get(block=False).term is QueryTerm.RBRACKET
+    assert tokens.get(block=False).term is QueryTerm.EOF
+
 
 def test_tokenizer_simple_where() -> None:
     raise NotImplementedError()
